b35bba19ea76c9f51fdee7e91594872a96122877
RAVINOALDI07 <pinoal2811@gmail.com>
2026-03-30 08:53:00 +0700

    del view

--- app/Views/auth/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-// public/auth/logout.php
-validateCsrf();
-logoutUser();
-setFlash('success', 'Anda berhasil logout.');
-redirect('/auth/login.php');
--- app/Views/notifications/mark_all.php
+++
@@ -1,9 +0,0 @@
-<?php
-// public/notifications/mark_all.php
-require_once BASE_PATH . '/helpers/notification_helper.php';
-
-requireLogin();
-markAllNotificationsRead(currentUserId());
-$referer = $_SERVER['HTTP_REFERER'] ?? (APP_URL . '/' . currentRole() . '/dashboard.php');
-header('Location: ' . $referer);
-exit;
